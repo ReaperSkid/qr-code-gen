652b21b6c3a677ff37c9d14e519a50defeaf92c8
Reaper <109579027+ReaperSkid@users.noreply.github.com>
2022-07-20 12:18:03 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-#QR CODE GENERATOR COMING SOON
+import qrcode
+
+QRCODEChoice = input("Your URL [-->]: ")
+
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(QRCODEChoice)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save('qrcode001.png')
