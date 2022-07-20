ddec2452b2d557b4b7b3efa3614e35c35a4f862a
Reaper <109579027+ReaperSkid@users.noreply.github.com>
2022-07-20 16:15:33 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import qrcode
 
-QRCODEChoice = input("Your URL [-->]: ")
-
-qr = qrcode.QRCode(
-        version=1,
-        box_size=10,
-        border=5)
-qr.add_data(QRCODEChoice)
-qr.make(fit=True)
-img = qr.make_image(fill='black', back_color='white')
-img.save('qrcode001.png')
+input_data = input("Enter link [-->]: ")
+        #Creating an instance of qrcode
+        qr = qrcode.QRCode(
+                version=1,
+                box_size=10,
+                border=5)
+        qr.add_data(input_data)
+        qr.make(fit=True)
+        img = qr.make_image(fill='black', back_color='white')
+        img.save("CreatedByScytheTool.png")
