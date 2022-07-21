18beb2c5284dbbb2485ee0ab0d75b132a284c314
Reaper <109579027+ReaperSkid@users.noreply.github.com>
2022-07-21 20:02:23 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import qrcode
+#MAKE SURE TO PIP INSTALL PILLOW
 
 input_data = input("Enter link [-->]: ")
-        #Creating an instance of qrcode
-        qr = qrcode.QRCode(
-                version=1,
-                box_size=10,
-                border=5)
-        qr.add_data(input_data)
-        qr.make(fit=True)
-        img = qr.make_image(fill='black', back_color='white')
-        img.save("CreatedByReaper#1234.png")
+#Creating an instance of qrcode
+qr = qrcode.QRCode(
+        version=1,
+        box_size=10,
+        border=5)
+qr.add_data(input_data)
+qr.make(fit=True)
+img = qr.make_image(fill='black', back_color='white')
+img.save("CreatedByReaper#1234.png")
